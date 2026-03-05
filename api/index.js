2b1ab0ddb5df514af00d929115ecbfc92c7470fc
Organización de ACDM <organizaciondeacdm@gmail.com>
2026-03-05 14:04:36 +0000

    actualizacion base de datos Versel propietaria

--- api/index.js
+++
@@ -1,8 +0,0 @@
-
-const app = require('../src/app');
-
-// Handler para Vercel Serverless Functions
-module.exports = app;
-
-// Exportar como handler explícito si es necesario
-module.exports.default = app;
--- client/src/acdm/components/fields/DateField.jsx
+++
@@ -1,8 +0,0 @@
-export function DateField({ field, value, onChange }) {
-  return (
-    <label className="field">
-      <span>{field.label}</span>
-      <input type="date" value={value || ''} onChange={(event) => onChange(field.name, event.target.value)} />
-    </label>
-  );
-}
