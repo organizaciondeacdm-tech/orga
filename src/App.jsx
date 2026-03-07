e0c32228921ed96f7455b85840aafcb517994c50
Organización de ACDM <organizaciondeacdm@gmail.com>
2026-03-07 17:01:42 +0000

    fix App

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@
 import { useState, useEffect, useCallback } from "react";
 import { getEscuelas, saveEscuelas, initializeKV } from './services/kvStorage.client.js';
 import "./Styles.css"; // Aquí es donde pegaste tu CSS gigante
-
-export default function App() 
-  const [escuelas, setEscuelas] = useState([]);
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
   const [loginForm, setLoginForm] = useState({ user: "", pass: "" });
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
 // 1. CARGA DESDE LA NUBE
